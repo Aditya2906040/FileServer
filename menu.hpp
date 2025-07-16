9016bbb4f4021277c2b6205e35baf73d0dcffadf
Aditya Raman <adityaarvind3@gmail.com>
2025-07-17 01:25:42 +0530

    First commit

---
+++ menu.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <iostream>
+#include <termios.h>
+#include <unistd.h>
+
+using namespace std;
+
+namespace Menu
+{
+    char my_getch()
+    {
+        struct termios oldt, newt;
+        char ch;
+
+        // Get current terminal settings
+        tcgetattr(STDIN_FILENO, &oldt);
+        newt = oldt;
+
+        // Disable canonical mode and echo
+        newt.c_lflag &= ~(ICANON | ECHO);
+
+        // Apply new settings immediately
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+        // Read one character
+        ch = getchar();
+
+        // Restore old settings
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
+        return ch;
+    }
+
+    void welcome()
+    {
+        cout << "------------------------------------------\n";
+        cout << "Welcome to The Ctrl+Xfer FileServer\n";
+        cout << "------------------------------------------\n";
+    }
+
+    void displayChoiceMenu()
+    {
+        cout << "1. List files\n2. Download files\n3. Upload files\n4. Exit" << endl;
+    }
+
+    bool typeCheck(char ch)
+    {
+        if (ch != '1' && ch != '2' && ch != '3' && ch != '4')
+        {
+            cout << "Choose correct input." << endl;
+            return true;
+        }
+        return false;
+    }
+
+    bool isesc(char ch)
+    {
+        if (ch == 27)
+            return true;
+        return false;
+    }
+}
