1b7a860389dea2baec880edd0d3e4b60accf4ed8
Aditya Raman <adityaarvind3@gmail.com>
2025-07-17 03:18:14 +0530

    List operation of fileops commiting

---
+++ server/file_ops.hpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include <vector>
+
+using namespace std;
+namespace fs = filesystem;
+
+namespace FileOps
+{
+    vector<string> files;
+    vector<string> directories;
+
+    void listContent()
+    {
+        string path = "./server_files";
+        for (const auto &entry : fs::directory_iterator(path))
+        {
+            if (entry.is_regular_file())
+            {
+                files.push_back(entry.path().filename().string());
+            }
+            else if (entry.is_directory())
+            {
+                directories.push_back(entry.path().filename().string());
+            }
+        }
+        cout << "\nFiles: \n";
+        if (files.empty())
+        {
+            cout << "(NONE)\n\n";
+        }
+        else
+        {
+            for (string file : files)
+            {
+                cout << "-" << file << endl;
+            }
+        }
+
+        cout << "\nDirectories" << endl;
+        if (!directories.empty())
+        {
+            for (string directory : directories)
+            {
+                cout << "-" << directory << endl;
+            }
+        }
+        else
+        {
+            cout << "(NONE)\n";
+        }
+    }
+
+}
